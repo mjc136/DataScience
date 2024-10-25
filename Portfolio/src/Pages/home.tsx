32233ccc874061052068b30e757843ec27a5d988
Michael Cullen <mjcullen2015@gmail.com>
2024-10-25 18:37:45 +0100

    added text to home page

--- Portfolio/src/Pages/home.tsx
+++ Portfolio/src/Pages/home.tsx
@@ -4,14 +4,34 @@ const Home: React.FC = () => {
     return (
         <>
             <div>
-                <h2>Home Page</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis, est nec aliquam congue, tellus metus vehicula dui,
-                    id vehicula urna nibh at eros. Morbi vel facilisis nisi. Pellentesque elit ante, malesuada eget lobortis id, finibus mollis diam.
-                    Donec vitae justo sit amet diam aliquam maximus. Proin fringilla lorem vitae lorem ornare dignissim. Phasellus pellentesque,
-                    lorem in elementum consectetur, nunc orci lobortis risus, vel sagittis sem lectus a ligula. Integer id neque accumsan felis ornare elementum ac ut dolor.
-                    Nullam et ultricies mauris. Duis accumsan sodales massa, vel efficitur libero pellentesque maximus. Curabitur ut felis consequat, vestibulum leo id, porta enim.
-                    Aliquam erat volutpat. Vestibulum eget tempor mi, non vulputate risus. Proin dolor magna, tempus quis maximus id, suscipit a nisi. Morbi rutrum malesuada nunc,
-                    aliquet vestibulum ante tristique id. Nam eu justo iaculis, tempor erat sed, accumsan quam. Suspendisse non rhoncus nibh.</p>
+                <h1>House Price Predictor</h1>
+                <h2>Data Source</h2>
+                <p>I got my data from the United Kingdom government datasets.</p>
+                <p>There is just over 141,000 rows in the data set ranging from 1995-2024</p>
+                <a href="https://www.gov.uk/government/statistical-data-sets/uk-house-price-index-data-downloads-january-2024">Here</a>
+                <br />
+                <h2>Pre-Processing</h2>
+                <p>I split the data 70/30 for training and testing.</p>
+                <p>When the user selects an area it will only use data from that area in it's calculations</p>
+                <h2>Data Understanding and visualisation</h2>
+                <p>To visualise the data I created a scatter plot graph.</p>
+                <p>I noticed during a major drop during 2008 during the market crash and a minor drop during the 202 covid pandemic.</p>
+                <br />
+                <h2>alghorithms</h2>
+                <p>I chose to use a linear regression model as house prices have steadily increased over the years</p>
+                <h2>Online Resources</h2>
+                <p>I Used the following documentation</p>
+                <a href="https://matplotlib.org/3.5.3/api/_as_gen/matplotlib.pyplot.html">matplotlib</a>
+                <br />
+                <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://numpy.org/doc/&ved=2ahUKEwiKkpzFiKqJAxWZXEEAHYicNEcQFnoECB0QAQ&usg=AOvVaw2a_3m0ZLpgxN-0uzQZP6YA">NumPy</a>
+                <br />
+                <a href="https://pandas.pydata.org/docs/">pandas</a>
+                <br />
+                <br />
+                <h2>Tools Used</h2>
+                <p>Python was the programming language i used as i am most comfortable with it </p>
+                <p>I used the following python libraries (Pandas, NumPy, Matplotlib, ipywidgets)</p>
+                <p>I used Jupyter Notebook for development and Github for version control</p>
             </div>
         </>
     );
